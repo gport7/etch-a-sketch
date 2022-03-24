da6fb3f0ebb1d28708f52dc79ef16d8ddcb8aea1
gport7 <gport@vt.edu>
2022-03-24 16:48:18 -0700

    makeGrid makes grid

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 let gridSize = prompt("Grid Size?");
+let grid = document.querySelector('.container__column');
+
+function makeGrid (size) {
+    console.log(size);
+    for (let i = 1; i <= size; i++) {
+        const column = document.createElement('div');
+        column.classList.add('column__pixel');
+        grid.appendChild(column);
+        for (let j = 1; j <= size; j++) {
+            const div = document.createElement('div');
+            div.classList.add('pixel', 'bg-secondary');
+            column.appendChild(div);
+        }
+    }    
+}
+
+makeGrid (gridSize);
